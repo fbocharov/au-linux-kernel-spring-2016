9ab8dcfadd906ffb9f1e691cee6dc8464b9f3401
Fyodor Bocharov <bocharovfedor@gmail.com>
2016-03-31 17:26:37 +0300

    Implemented hungry memory allocation task.

--- tasks/virt_mem_max/main.c
+++ tasks/virt_mem_max/main.c
@@ -1,5 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
 int main()
 {
-    // TODO
+	size_t bytesCount = 0;
+	size_t unit = -1;
+
+	while (unit > 0) {
+		if (MAP_FAILED == mmap(NULL, unit, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0))
+			unit /= 2;
+		else
+			bytesCount += unit;
+	}
+
+	printf("Max mem size = %ld bytes.\n", bytesCount);
     return 0;
 }
