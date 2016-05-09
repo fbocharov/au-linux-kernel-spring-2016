ad8252cf72a53a241e4fd8dd221ff4ea61ae491d
Fyodor Bocharov <bocharovfedor@gmail.com>
2016-05-09 19:53:41 +0300

    Implemented userspace mutex hw
    mutex_lib -- userspace interface.
    mutex_module -- kernel support for mutex_lib.

--- tasks/mutex/mutex_module/mutex_ioctl.h
+++ tasks/mutex/mutex_module/mutex_ioctl.h
@@ -1,5 +1,5 @@
-#ifndef _VSD_UAPI_H
-#define _VSD_UAPI_H
+#ifndef _MUTEX_UAPI_H
+#define _MUTEX_UAPI_H
 
 #ifdef __KERNEL__
 #include <asm/ioctl.h>
@@ -33,12 +33,12 @@ typedef struct mutex_ioctl_lock_wake_arg {
 } mutex_ioctl_lock_wake_arg_t;
 
 #define MUTEX_IOCTL_LOCK_CREATE \
-    _IOR(MUTEX_IOCTL_MAGIC, 1, mutex_ioctl_lock_create_arg_t)
+    _IOW(MUTEX_IOCTL_MAGIC, 1, mutex_ioctl_lock_create_arg_t)
 #define MUTEX_IOCTL_LOCK_DESTROY \
     _IOR(MUTEX_IOCTL_MAGIC, 2, mutex_ioctl_lock_destroy_arg_t)
 #define MUTEX_IOCTL_LOCK_WAIT \
-    _IOR(MUTEX_IOCTL_MAGIC, 3, mutex_ioctl_lock_wait_arg_t)
+    _IOW(MUTEX_IOCTL_MAGIC, 3, mutex_ioctl_lock_wait_arg_t)
 #define MUTEX_IOCTL_LOCK_WAKE \
     _IOW(MUTEX_IOCTL_MAGIC, 4, mutex_ioctl_lock_wake_arg_t)
 
-#endif //_VSD_UAPI_H
+#endif //_MUTEX_UAPI_H
